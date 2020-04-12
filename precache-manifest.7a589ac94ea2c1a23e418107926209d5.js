db9ea80c567bf9d39d9bbace9f1134e05e25be6b
hhy666 <hhy666@uw.edu>
2020-04-12 00:44:26 -0700

    Updates

---
+++ precache-manifest.7a589ac94ea2c1a23e418107926209d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3a5d4e5ef5fb7d312d3008b9332619",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "46617afa5c0355398cc1",
+    "url": "/monsters-relodex/static/css/main.1157ca54.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-relodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46617afa5c0355398cc1",
+    "url": "/monsters-relodex/static/js/main.07bc66c7.chunk.js"
+  },
+  {
+    "revision": "f5ccbf1227d77bbfa110",
+    "url": "/monsters-relodex/static/js/runtime-main.42d51eb0.js"
+  }
+]);
